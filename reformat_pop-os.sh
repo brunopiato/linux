c4b784dc7e1e0b16cbf76a2ecc51d5c4c9248d80
bruno@pop-os <piatobio@gmail.com>
2023-03-20 19:06:43 -0300

    update reformat including install_extensions and write_ext_list

--- reformat_pop-os.sh
+++ reformat_pop-os.sh
@@ -229,84 +229,23 @@ echo "alias upd='sudo apt update && sudo apt upgrade'" >> ~/.bash_aliases
 
 
 
-
-
-
-
-#-----------------------------------------------------------------------------------------
-# Criando um arquivo com uma lista de extensões a serem baixadas
-#-----------------------------------------------------------------------------------------
-
-touch ~/lista_ext.txt
-
-echo "
-# ArcMenu
-Adiciona um menu tipo Menu Iniciar do Windows e tem muitos icones legais
-https://extensions.gnome.org/extension/3628/arcmenu/
-
-# Aylur's Widgets
-Esta extensão adiciona uma série de wigdets à área de trabalho e à barra de tarefas.
-https://extensions.gnome.org/extension/5338/aylurs-widgets/
-
-# Control Blur Effect On Lock Screen
-Permite o plano de fundo aparecer na tela de bloqueio
-https://extensions.gnome.org/extension/2935/control-blur-effect-on-lock-screen/
-
-# Dash to dock for Cosmic
-Um painel de ajustes para a doca do Pop_OS
-https://extensions.gnome.org/extension/5004/dash-to-dock-for-cosmic/
-
-# Just Perfection
-Adiciona diversos controles de ajustes da interface
-https://extensions.gnome.org/extension/3843/just-perfection/
-
-# Maximize to workspace with history
-Quando janelas são maximizadas, elas pulam de workspace
-https://extensions.gnome.org/extension/2857/maximize-to-workspace-with-history/
-
-# OpenWeather
-Extensão de previsão meteorológica
-https://extensions.gnome.org/extension/750/openweather/
-
-# Rounded Window Corners
-Arredonda as quinas das janelas
-https://github.com/yilozt/rounded-window-corners
-
-# Show Desktop Button
-Adicionar botão de visualizar o desktop
-https://extensions.gnome.org/extension/1194/show-desktop-button/
-
-# Switch workspace
-Facilita a mudança entre workspaces
-https://extensions.gnome.org/extension/1231/switch-workspace/
-
-# Time ++
-Adiciona um timer com cronômetro e pomodoros
-https://extensions.gnome.org/extension/1238/time/
-
-# Unite
-A extensão Unite permite arrumar visualmente o ambiente Ubuntu, ajustando a aparência da interface.
-https://extensions.gnome.org/extension/1287/unite/
-
-# GNOME Clipboard
-CLipboard do GNOME
-https://extensions.gnome.org/extension/4422/gnome-clipboard/
-
-" >> ~/lista_ext.txt
-
-
-
-
-
-
 #-----------------------------------------------------------------------------------------
 # Instalando as exntensões GNOME
 #-----------------------------------------------------------------------------------------
 
-
-
-
-
+sleep 3
+var=$(pwd)
+echo "ESTAMOS TERMINANDO A INSTALAÇÃO! VOCÊ GOSTARIA DE INSTALAR AS SEGUINTES EXTENSÕES DO GNOME-SHELL:"
+ls $var/extencoes_gnome42
+sleep 2
+echo "INSTALAR AGORA?[S/N]: "
+read resposta
+if [ $resposta == "S" -o $resposta == "s" ] ; then
+bash ./install_extensions.sh
+else sleep 2 
+echo "OK. VOCÊ PODE INSTALAR MAIS TARDE A PARTIR DA LISTA DE EXTENSÕES CRIADA NA SUA PASTA PESSOAL."
+bash ./write_ext_list.sh
+fi
 
 
 
---
+++ write_ext_list.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+touch ~/lista_ext.txt
+
+echo "
+# ArcMenu
+Adiciona um menu tipo Menu Iniciar do Windows e tem muitos icones legais
+https://extensions.gnome.org/extension/3628/arcmenu/
+
+# Aylur's Widgets
+Esta extensão adiciona uma série de wigdets à área de trabalho e à barra de tarefas.
+https://extensions.gnome.org/extension/5338/aylurs-widgets/
+
+# Control Blur Effect On Lock Screen
+Permite o plano de fundo aparecer na tela de bloqueio
+https://extensions.gnome.org/extension/2935/control-blur-effect-on-lock-screen/
+
+# Dash to dock for Cosmic
+Um painel de ajustes para a doca do Pop_OS
+https://extensions.gnome.org/extension/5004/dash-to-dock-for-cosmic/
+
+# Just Perfection
+Adiciona diversos controles de ajustes da interface
+https://extensions.gnome.org/extension/3843/just-perfection/
+
+# Maximize to workspace with history
+Quando janelas são maximizadas, elas pulam de workspace
+https://extensions.gnome.org/extension/2857/maximize-to-workspace-with-history/
+
+# OpenWeather
+Extensão de previsão meteorológica
+https://extensions.gnome.org/extension/750/openweather/
+
+# Rounded Window Corners
+Arredonda as quinas das janelas
+https://github.com/yilozt/rounded-window-corners
+
+# Show Desktop Button
+Adicionar botão de visualizar o desktop
+https://extensions.gnome.org/extension/1194/show-desktop-button/
+
+# Switch workspace
+Facilita a mudança entre workspaces
+https://extensions.gnome.org/extension/1231/switch-workspace/
+
+# Time ++
+Adiciona um timer com cronômetro e pomodoros
+https://extensions.gnome.org/extension/1238/time/
+
+# Unite
+A extensão Unite permite arrumar visualmente o ambiente Ubuntu, ajustando a aparência da interface.
+https://extensions.gnome.org/extension/1287/unite/
+
+# GNOME Clipboard
+CLipboard do GNOME
+https://extensions.gnome.org/extension/4422/gnome-clipboard/
+
+" >> ~/lista_ext.txt
+
