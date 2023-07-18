23d7401f1849406b5adac93c30d07f8113bfd1cd
brunopiato <piatobio@gmail.com>
2023-07-18 14:47:24 -0300

    updating reformat PopPS

--- PopOS/reformat_PopOS.sh
+++ PopOS/reformat_PopOS.sh
@@ -48,7 +48,7 @@ sudo apt install onlyoffice-desktopeditors -y
 
 ## Instalando IriunWebcam
 wget https://iriun.gitlab.io/iriunwebcam-2.8.deb
-sudo apto install ./iriunwebcam-2.8.deb
+sudo apt install ./iriunwebcam-2.8.deb -y
 sudo rm ./iriunwebcam-2.8.deb
 
 ## Instalando utilitários do plank para substituir a doca
@@ -84,7 +84,7 @@ sudo snap install inkscape #Inkscape
 sudo snap install notion-snap-reborn #Notion
 sudo apt install vlc -y #VLC Media Player
 sudo snap install emote
-sudo snap install obsidian
+sudo apt install obsidian
 
 ## Instalando Dropbox
 curl https://linux.dropbox.com/packages/ubuntu/dropbox_2020.03.04_amd64.deb --output dropbox_2020.03.04_amd64.deb
