b12246b0d4b43c0890b4203b742a5d8f8dd9c11c
brunopiato <piatobio@gmail.com>
2023-07-29 11:41:35 -0300

    adding ubuntu configuration

--- desktop_shorcuts.sh
+++ desktop_shorcuts.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 echo "VOCÊ GOSTARIA DE COPIAR OS ATALHOS OU REDEFINIR OS ATALHOS?"
-read -p "[C]Copiar 
-[R]Redefinir
+read -p "[C]Copiar as configuraçoes do PC para o arquivo
+[R]Redefinir as configuraçoes do PC a partir de um arquivo
 " resposta
 
 if [ "$resposta" == "C" ]; then 
     dconf dump '/org/gnome/desktop/wm/keybindings/' > keybindings.dconf
-    dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > custom-keybindings.dconf
+    dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > /custom-keybindings.dconf
 elif [ "$resposta" == "R" ]; then 
     dconf load '/org/gnome/desktop/wm/keybindings/' < keybindings.dconf
-    dconf load '/org/gnome/settings-daemon/plugins/media-keys/    custom-keybindings/' < custom-keybindings.dconf
+    dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < custom-keybindings.dconf
+    dconf update
 else echo "Fim"
 fi
