6a50fcfd3eb64b92e3e6670d46b12b0128f226e0
brunopiato <piatobio@gmail.com>
2026-02-05 16:33:42 -0300

    comentado código do docker no ubuntu24 pois pode dar conflito com o DE

--- Ubuntu24/reformat_Ubuntu24.sh
+++ Ubuntu24/reformat_Ubuntu24.sh
@@ -82,32 +82,32 @@ echo "Versão Python: $(python --version)"
 #-----------------------------------------------------------------------------------------
 # Instalando Docker
 #-----------------------------------------------------------------------------------------
-echo "Iniciando a instalação do Docker"
-sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+#echo "Iniciando a instalação do Docker"
+#sudo apt remove docker docker-engine docker.io containerd runc
+#sudo apt install -y \
+#    ca-certificates \
+#    curl \
+#    gnupg \
+#    lsb-release
     
-sudo mkdir -p /etc/apt/keyrings
+#sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+#  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-  https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" \
-  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+#  https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" \
+#  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
+#sudo apt update
 
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -aG docker $USER
+#sudo usermod -aG docker $USER
 
 
 #-----------------------------------------------------------------------------------------
